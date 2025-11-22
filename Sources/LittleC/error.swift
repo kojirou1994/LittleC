b2aa4cb637defc0978672c1cffa143969b0d4bda
kojirou <kojirouhtc@gmail.com>
2025-11-23 00:10:40 +0800

    update

--- Sources/LittleC/error.swift
+++ Sources/LittleC/error.swift
@@ -1,13 +1,13 @@
 #if canImport(Darwin) || os(FreeBSD)
 @_extern(c)
 @_alwaysEmitIntoClient @inlinable @inline(__always)
-func __error() -> UnsafeMutablePointer<CInt>
+public func __error() -> UnsafeMutablePointer<CInt>
 #elseif os(Linux)
 // glibc: https://www.gnu.org/software/libc/manual/html_node/Checking-for-Errors.html
 // musl: same
 @_extern(c, "__errno_location")
 @_alwaysEmitIntoClient @inlinable @inline(__always)
-func __error() -> UnsafeMutablePointer<CInt>
+public func __error() -> UnsafeMutablePointer<CInt>
 #else
 #error("errno todo")
 #endif
@@ -21,3 +21,11 @@ public var errno: CInt {
     __error().pointee = newValue
   }
 }
+
+@_extern(c)
+@_alwaysEmitIntoClient @inlinable @inline(__always)
+public func perror(_ string: UnsafePointer<CChar>)
+
+@_extern(c)
+@_alwaysEmitIntoClient @inlinable @inline(__always)
+public func strerror(_ e: CInt) -> UnsafePointer<CChar>
