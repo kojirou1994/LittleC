5fcb15c20a704cc6f854f747981ae1281bd2b724
kojirou <kojirouhtc@gmail.com>
2025-11-26 11:42:57 +0800

    update

--- Sources/BigC/include/BigC.h
+++ Sources/BigC/include/BigC.h
@@ -1,36 +1,184 @@
 #include <time.h>
+#if __has_include(<sys/errno.h>)
+#include <sys/errno.h>
+#else
 #include <errno.h>
+#endif
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
+#include <fts.h>
 
-#define __swift_nonisolated_unsafe              __attribute__((__swift_attr__("nonisolated(unsafe)")))
 
-int static inline little_open(const char *path, int oflag, mode_t mode) {
+#define SWIFT_NONISOLATED           __attribute__((__swift_attr__("nonisolated(unsafe)")))
+#define SWIFT_INLINE                static inline __attribute__((__always_inline__))
+
+SWIFT_INLINE int swift_open(const char *path, int oflag, mode_t mode) {
   return open(path, oflag, mode);
 }
 
-int static inline little_openat(int fd, const char *path, int oflag, mode_t mode) {
+SWIFT_INLINE int swift_openat(int fd, const char *path, int oflag, mode_t mode) {
   return openat(fd, path, oflag, mode);
 }
 
-#ifdef __apple__
+#ifdef __APPLE__
 
-extern void exit(int) __attribute__((noreturn));
-extern void _exit(int) __attribute__((noreturn));
-extern void _Exit(int) __attribute__((noreturn));
+#include <libproc.h>
+#include <removefile.h>
+
+SWIFT_INLINE unsigned long swift_FIOSETOWN() {
+  return FIOSETOWN;
+}
+
+SWIFT_INLINE unsigned long swift_FIOGETOWN() {
+  return FIOGETOWN;
+}
+
+SWIFT_INLINE unsigned long swift_FIODTYPE() {
+  return FIODTYPE;
+}
 
 #endif
 
 #ifdef __linux__
 
-extern FILE *stdin __swift_nonisolated_unsafe;
-extern FILE *stdout __swift_nonisolated_unsafe;
-extern FILE *stderr __swift_nonisolated_unsafe;
+extern FILE *stdin SWIFT_NONISOLATED;
+extern FILE *stdout SWIFT_NONISOLATED;
+extern FILE *stderr SWIFT_NONISOLATED;
+extern char **environ SWIFT_NONISOLATED;
 
 #endif
 
 extern void pthread_exit(void*) __attribute__((noreturn));
+
+SWIFT_INLINE int swift_WIFEXITED(int status) {
+  return WIFEXITED(status);
+}
+
+SWIFT_INLINE int swift_WIFSIGNALED(int status) {
+  return WIFSIGNALED(status);
+}
+
+SWIFT_INLINE int swift_WIFSTOPPED(int status) {
+  return WIFSTOPPED(status);
+}
+
+SWIFT_INLINE int swift_WEXITSTATUS(int status) {
+  return WEXITSTATUS(status);
+}
+
+SWIFT_INLINE int swift_WTERMSIG(int status) {
+  return WTERMSIG(status);
+}
+
+SWIFT_INLINE int swift_WCOREDUMP(int status) {
+  return WCOREDUMP(status);
+}
+
+SWIFT_INLINE int swift_WSTOPSIG(int status) {
+  return WSTOPSIG(status);
+}
+
+SWIFT_INLINE int swift_WIFCONTINUED(int status) {
+  return WIFCONTINUED(status);
+}
+
+SWIFT_INLINE int swift_W_EXITCODE(int ret, int sig) {
+  return W_EXITCODE(ret, sig);
+}
+
+SWIFT_INLINE int swift_W_STOPCODE(int sig) {
+  return W_STOPCODE(sig);
+}
+
+SWIFT_INLINE unsigned long swift_FIOCLEX() {
+  return FIOCLEX;
+}
+
+SWIFT_INLINE unsigned long swift_FIONCLEX() {
+  return FIONCLEX;
+}
+
+SWIFT_INLINE unsigned long swift_FIONREAD() {
+  return FIONREAD;
+}
+
+SWIFT_INLINE unsigned long swift_FIONBIO() {
+  return FIONBIO;
+}
+
+SWIFT_INLINE unsigned long swift_FIOASYNC() {
+  return FIOASYNC;
+}
+
+SWIFT_INLINE rlim_t swift_RLIM_INFINITY() {
+  return RLIM_INFINITY;
+}
+
+SWIFT_INLINE int swift_RLIMIT_MEMLOCK() {
+  return RLIMIT_MEMLOCK;
+}
+
+SWIFT_INLINE int swift_RLIMIT_NPROC() {
+  return RLIMIT_NPROC;
+}
+
+SWIFT_INLINE int swift_RLIMIT_RSS() {
+  return RLIMIT_RSS;
+}
+
+SWIFT_INLINE void swift_clearerr_unlocked(FILE *stream) {
+  return clearerr_unlocked(stream);
+}
+
+SWIFT_INLINE int swift_feof_unlocked(FILE *stream) {
+  return feof_unlocked(stream);
+}
+
+SWIFT_INLINE int swift_ferror_unlocked(FILE *stream) {
+  return ferror_unlocked(stream);
+}
+
+SWIFT_INLINE int swift_fileno_unlocked(FILE *stream) {
+  return fileno_unlocked(stream);
+}
+
+SWIFT_INLINE u_int32_t swift_major(dev_t dev) {
+  return major(dev);
+}
+
+SWIFT_INLINE u_int32_t swift_minor(dev_t dev) {
+  return minor(dev);
+}
+
+SWIFT_INLINE dev_t swift_makedev(u_int32_t major, u_int32_t minor) {
+  return makedev(major, minor);
+}
+
+SWIFT_INLINE int swift_get_errno() {
+  return errno;
+}
+
+SWIFT_INLINE void swift_set_errno(int value) {
+  errno = value;
+}
+
+SWIFT_INLINE FILE* swift_get_stdin() {
+  return stdin;
+}
+
+SWIFT_INLINE FILE* swift_get_stdout() {
+  return stdout;
+}
+
+SWIFT_INLINE FILE* swift_get_stderr() {
+  return stderr;
+}
--- Sources/LittleC/pthread.swift
+++ Sources/LittleC/pthread.swift
@@ -1,8 +1,8 @@
 import BigC
 
-@_extern(c)
-@_alwaysEmitIntoClient @inlinable @inline(__always)
-public func pthread_join(_: pthread_t, _: UnsafeMutablePointer<UnsafeMutableRawPointer?>? = nil) -> Int32
+//@_extern(c)
+//@_alwaysEmitIntoClient @inlinable @inline(__always)
+//public func pthread_join(_: pthread_t, _: UnsafeMutablePointer<UnsafeMutableRawPointer?>? = nil) -> Int32
 
 @_extern(c)
 @_alwaysEmitIntoClient @inlinable @inline(__always)
