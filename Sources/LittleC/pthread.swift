279fd5a0c5a2f70fa5d3698e5dc915fa76c7e8b1
kojirou <kojirouhtc@gmail.com>
2025-11-29 05:24:03 +0800

    update

--- Sources/LittleC/pthread.swift
+++ Sources/LittleC/pthread.swift
@@ -1,9 +1,1 @@
 import BigC
-
-//@_extern(c)
-//@_alwaysEmitIntoClient @inlinable @inline(__always)
-//public func pthread_join(_: pthread_t, _: UnsafeMutablePointer<UnsafeMutableRawPointer?>? = nil) -> Int32
-
-@_extern(c)
-@_alwaysEmitIntoClient @inlinable @inline(__always)
-public func pthread_once(_: UnsafeMutablePointer<pthread_once_t>, _: @convention(c) () -> ()) -> Int32
