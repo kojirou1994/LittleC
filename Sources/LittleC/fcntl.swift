0135bcb6752b9f19aed59e5999dc95e6eee642ba
kojirou <kojirouhtc@gmail.com>
2025-11-28 21:16:57 +0800

    update
    open & openat

--- Sources/LittleC/fcntl.swift
+++ Sources/LittleC/fcntl.swift
@@ -1,6 +1,21 @@
 import BigC
 
 @_alwaysEmitIntoClient @inlinable @inline(__always)
+public func open(_ path: UnsafePointer<CChar>, _ oflag: Int32,_ mode: mode_t) -> Int32 {
+  swift_open(path, oflag, mode)
+}
+
+@_alwaysEmitIntoClient @inlinable @inline(__always)
+public func openat(_ fd: Int32, _ path: UnsafePointer<CChar>, _ oflag: Int32) -> Int32 {
+  swift_openat(fd, path, oflag, 0)
+}
+
+@_alwaysEmitIntoClient @inlinable @inline(__always)
+public func openat(_ fd: Int32, _ path: UnsafePointer<CChar>, _ oflag: Int32, _ mode: mode_t) -> Int32 {
+  swift_openat(fd, path, oflag, mode)
+}
+
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public func fcntl(_ fd: CInt, _ cmd: CInt) -> CInt {
   BigC.swift_fcntl(fd, cmd)
 }
