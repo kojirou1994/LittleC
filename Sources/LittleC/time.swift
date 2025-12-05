861444dfe0bec375ae5395d97b4437fe419f9777
kojirou <kojirouhtc@gmail.com>
2025-12-06 06:13:08 +0800

    fix
    add missing public

--- Sources/LittleC/time.swift
+++ Sources/LittleC/time.swift
@@ -3,5 +3,5 @@ import BigC
 #if os(Linux)
 @_extern(c)
 @_alwaysEmitIntoClient @inlinable @inline(__always)
-func gettimeofday(_: UnsafeMutablePointer<timeval>?, _: UnsafeMutablePointer<timezone>?) -> Int32
+public func gettimeofday(_: UnsafeMutablePointer<timeval>?, _: UnsafeMutablePointer<timezone>?) -> Int32
 #endif
