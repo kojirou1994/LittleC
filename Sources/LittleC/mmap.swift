a42ca0de17cafdb9d6ad704d98e6c7e1d3d95887
kojirou <kojirouhtc@gmail.com>
2026-04-15 19:22:08 +0800

    add MAP_FAILED

---
+++ Sources/LittleC/mmap.swift
@@ -0,0 +1,2 @@
+@_alwaysEmitIntoClient @inlinable @inline(__always)
+public var MAP_FAILED: UnsafeMutableRawPointer? { .init(bitPattern: -1) }
--- Sources/LittleC/signal.swift
+++ Sources/LittleC/signal.swift
@@ -1,9 +1,13 @@
 import BigC
 
 #if UNIX_BSD
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_DFL: sig_t? { nil }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_IGN: sig_t { unsafeBitCast(1, to: sig_t.self) }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_ERR: sig_t { unsafeBitCast(-1, to: sig_t.self) }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_HOLD: sig_t {
   #if APPLE
   unsafeBitCast(5, to: sig_t.self)
@@ -16,13 +20,17 @@ public var SIG_HOLD: sig_t {
 public typealias sighandler_t = __sighandler_t
 #endif
 
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_DFL: sighandler_t? { return nil }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_IGN: sighandler_t {
   unsafeBitCast(1, to: sighandler_t.self)
 }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_ERR: sighandler_t {
   unsafeBitCast(-1, to: sighandler_t.self)
 }
+@_alwaysEmitIntoClient @inlinable @inline(__always)
 public var SIG_HOLD: sighandler_t {
   unsafeBitCast(2, to: sighandler_t.self)
 }
